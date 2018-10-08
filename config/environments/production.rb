3493d6c6ce011c05f00c62a8ed1f07c41bb7c7c4
Yogen Aggarwal <in.yogen@gmail.com>
2018-10-08 22:15:57 +0000

    Fix autoload paths for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   
-  config.eager_load_paths << "#{Rails.root}/lib"
+  config.eager_load_paths += %W( #{Rails.root}/lib )
   
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
